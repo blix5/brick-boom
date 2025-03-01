8ee0aabf48a86e74c4e8d7656ab893861c054d8c
blueberryperson5 <peter.lour@icloud.com>
2025-02-28 17:43:38 -0800

    main files

---
+++ src/Main.jsx
@@ -0,0 +1,15 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        <p>
+          BRICK BOOM
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
